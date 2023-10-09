2b8f66b98ee88b4cdd49520b988f7207019bcfbb
mohyerolo <garam2101@naver.com>
2023-10-09 14:37:17 +0900

    fix: passwordEncode 빈으로 추가

--- src/main/java/com/clone/instagram/global/config/SecurityConfig.java
+++ src/main/java/com/clone/instagram/global/config/SecurityConfig.java
@@ -1,56 +1,54 @@
 package com.clone.instagram.global.config;
 
-import com.clone.instagram.global.auth.jwt.JwtAccessDeniedHandler;
-import com.clone.instagram.global.auth.jwt.JwtAuthenticationEntryPoint;
 import com.clone.instagram.global.auth.jwt.JwtFilter;
 import com.clone.instagram.global.auth.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtProvider jwtProvider;
-    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
-    private final JwtAuthenticationEntryPoint authenticationEntryPoint;
-    private final RedisTemplate<String, Object> redisTemplate;
 
     private static final String[] PERMIT_ALL_PATTERNS = new String[]{
             "/",
             "/auth/**",
-            "/WEB-INF/views/**",
             "/css/**",
             "/images/**",
             "/favicon.ico"
     };
 
     @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
+                .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers(Stream
-                                .of(PERMIT_ALL_PATTERNS)
-                                .map(AntPathRequestMatcher::antMatcher)
-                                .toArray(AntPathRequestMatcher[]::new)).permitAll()
-                        .anyRequest().authenticated())
-                .exceptionHandling(e -> { e.authenticationEntryPoint(authenticationEntryPoint); e.accessDeniedHandler(jwtAccessDeniedHandler);})
-                .formLogin(formLogin -> formLogin.loginPage("/auth/loginForm").permitAll().defaultSuccessUrl("/"))
-                .addFilterBefore(new JwtFilter(jwtProvider, redisTemplate), UsernamePasswordAuthenticationFilter.class)
+                        .requestMatchers(
+                                Arrays.stream(PERMIT_ALL_PATTERNS).map(AntPathRequestMatcher::antMatcher).toArray(AntPathRequestMatcher[]::new)
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
                 .build();
-
     }
 }
