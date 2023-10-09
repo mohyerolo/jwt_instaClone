d83916505c087730efcb41ab00f7eff87dd50ba1
mohyerolo <garam2101@naver.com>
2023-10-09 14:38:11 +0900

    feat: 사용자 이름 가져오는 util 추가

---
+++ src/main/java/com/clone/instagram/global/util/SecurityUtil.java
@@ -0,0 +1,17 @@
+package com.clone.instagram.global.util;
+
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@NoArgsConstructor
+public class SecurityUtil {
+    public static String getUserName() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication.getName() == null) {
+            throw new RuntimeException("Security Context에 인증 정보가 없습니댜.");
+        }
+        return authentication.getName();
+    }
+
+}
